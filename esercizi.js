a8359b895148b58b8e158f7a15d94e5d96532e07
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-02-27 10:27:42 +0100

    esercizi

--- esercizi.js
+++ esercizi.js
@@ -1,7 +1,22 @@
 /*esercizio for*/
 /*a%b se da 0, l'if ritorna false*/
-/*potevo usare anche NOT mettendo !davanti alla parentesi tonda della condizione dell'if*/ for (var i=1; i<=100; i=i+2){
+/*potevo usare anche NOT mettendo !davanti alla parentesi dell'if*/
+for (var i=1; i<=100; i=i+2){
     if (i%5==0) {
-        console.log(i + " è multiplo di 5")
+    console.log(i + " è multiplo di 5")
     }
 }
+    
+    
+/*esercizio paridispari*/
+var n=11;
+var d=2;
+var i=1;
+while (d*i<=n) {
+    i=i+1;
+};
+if(d*i==n){
+    console.log("il numero " + n + " diviso per " + d + " non da resto quindi è paro")
+} else{
+    console.log("il numero " + n + " diviso per " + d + " da resto quindi è disparo")
+};
