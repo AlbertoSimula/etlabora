a102149c0c5c82880d4b172435ab7bfee1b8a761
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-02-27 09:33:13 +0100

    "esercizi"

---
+++ esercizi.js
@@ -0,0 +1,7 @@
+/*esercizio for*/
+/*a%b se da 0, l'if ritorna false*/
+/*potevo usare anche NOT mettendo !davanti alla parentesi tonda della condizione dell'if*/ for (var i=1; i<=100; i=i+2){
+    if (i%5==0) {
+        console.log(i + " è multiplo di 5")
+    }
+}
---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+var x=0;
+var y=0;
+var sum = x+y;
+for (var i =0; i<=10; i=i+1) {
+    console.log(sum[i]);
+    x=y;
+    y=sum;
+}
+
