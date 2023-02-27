39823b8b25c3413880a21b7b8c19b314b29a3a8e
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-02-27 14:38:03 +0100

    terzo

--- terzofile.js
+++ terzofile.js
@@ -1,2 +1,12 @@
 console.log(dog);
 var dog = 3;
+/*stiamo iniziando a valutare come il js engine 
+legge lo script: Analisi = legge quelle linee dove ci sono parole chiave; 
+tutte le var e le function "vengono portate in cima"
+es.
+var dog; crea variabile, ma senza valore => undefined
+console.log(dog);
+var dog = 3;
+
+Processo chiamato Hoistin
+*/
