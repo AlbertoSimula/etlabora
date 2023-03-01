cab076a297984b0cbcbacec4354b59bea9744659
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-01 22:09:37 +0100

    new

--- esercizioremove.js
+++ esercizioremove.js
@@ -1,14 +1,13 @@
+
+
 var listButton = document.body.querySelectorAll('table td button');
 /*var i = prompt(("dimmi che bottone vuoi togliere"),"")*/
 
-
-for(var i=0; i<listButton.length; i=i+1){
-    console.log(elem = listButton[i]);
-    var par = elem.parentNode.parentNode;
-    console.log(par);
-    function remover(){
-        par.remove();
-    };
+var i =0;
+console.log(elem = listButton[i]);
+var par = elem.parentNode.parentNode;
+console.log(par);
+function remover(){
+    par.remove();
 };
 var es = elem.addEventListener("click", remover);
-
---
+++ prove.js
@@ -0,0 +1,11 @@
+var listButton = document.body.querySelectorAll('table td button');
+for(var i = 0; i<listButton.length; i=i+1){
+    console.log(i);
+    console.log(elem = listButton[i]);
+    var par = elem.parentNode.parentNode;
+    console.log(par);
+};
+function remover(){
+    par.remove();
+};
+var es = elem.addEventListener("click", remover);
