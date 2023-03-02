0bc1cf389e4345b61c6cb8f9bcb3b7b3bc6574c3
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-02 22:18:18 +0100

    patch

--- prove.js
+++ prove.js
@@ -1,9 +1,10 @@
 var listButton = document.body.querySelectorAll('table td button');
-
-    console.log(elem = listButton.children);
+for(var i = 0; i<listButton.length; i=i+1){
+    console.log(i);
+    console.log(elem = listButton[i]);
     var par = elem.parentNode.parentNode;
     console.log(par);
-;
+};
 elem.addEventListener("click", ()=>{
     par.remove();
 });
