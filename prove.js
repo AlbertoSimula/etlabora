8fc7891122e1c2e9769b4b3884a5942effce1aa4
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-02 23:13:23 +0100

    patch

--- prove.js
+++ prove.js
@@ -1,11 +1,15 @@
-var listButton = document.body.querySelectorAll('table td button');
+/*il problema è che il for-loop arriva all'ultimo elemento e tiene quel valore dell'indice
+ *della collection per togliere la riga corrispondente. Come risolvere? Vedremo*/
+
+
+
+var listButton = document.body.querySelectorAll('.btn');
 for(var i = 0; i<listButton.length; i=i+1){
     console.log(i);
     console.log(elem = listButton[i]);
     var par = elem.parentNode.parentNode;
     console.log(par);
-};
-function remover(){
+}; 
+var es = elem.addEventListener("click", () => {
     par.remove();
-};
-var es = elem.addEventListener("click", remover);
+});
