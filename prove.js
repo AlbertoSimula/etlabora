e483363da688b81834d89db92ea96bcc96084298
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-02 16:37:52 +0100

    new

--- prove.js
+++ prove.js
@@ -1,11 +1,9 @@
 var listButton = document.body.querySelectorAll('table td button');
-var i = 0;
-while(i<listButton.length){
+for(var i = 0; i<listButton.length; i=i+1){
     console.log(i);
     console.log(elem = listButton[i]);
     var par = elem.parentNode.parentNode;
     console.log(par);
-    i=i+1;
 };
 function remover(){
     par.remove();
