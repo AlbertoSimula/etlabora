bac2f119df61d483e4ca93f24b392ca5b658a305
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-03 08:22:30 +0100

    patch

--- prove.js
+++ prove.js
@@ -1,8 +1,6 @@
 /*il problema è che il for-loop arriva all'ultimo elemento e tiene quel valore dell'indice
  *della collection per togliere la riga corrispondente. Come risolvere? Vedremo*/
-
-
-
+/*
 var listButton = document.body.querySelectorAll('.btn');
 for(var i = 0; i<listButton.length; i=i+1){
     console.log(i);
@@ -13,3 +11,16 @@ for(var i = 0; i<listButton.length; i=i+1){
 var es = elem.addEventListener("click", () => {
     par.remove();
 });
+*/
+/*soluzione*/
+
+var listButton = document.body.querySelectorAll('.btn');
+
+for(var i = 0; i<listButton.length; i=i+1){
+    listButton[i].addEventListener("click", function(e){
+        e.target.parentElement.parentElement.remove();
+    })
+}; 
+/* e sempre per mettere l'argomento come parametro della funzione
+ * target prende solamente l'elemento che subisce l'evento
+ */
