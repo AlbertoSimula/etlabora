0df73e51d6af408fe986764fcedb46722e7241ac
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-03 16:18:57 +0100

    aggiunte librerie

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,20 @@ const path = require('path');
 module.exports = {
     entry: './esercizi.js', /*di solito è index.js*/
     mode: 'development',
+    module: {
+        rules: [
+        {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+            presets: ['@babel/preset-env']
+            }
+        }
+        }
+    ]
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js',
