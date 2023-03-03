f20c42b8d11788040f9a09fcaa0d1ef6b95cbe63
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-03 15:59:45 +0100

    "aggiunte librerie"

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/esercizi.js', /*di solito è index.js*/
+    entry: './esercizi.js', /*di solito è index.js*/
+    mode: 'development',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js',
