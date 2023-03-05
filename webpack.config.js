c64dbb4f18e51ba82a7b88552ad53e74ffc8c421
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-05 22:18:26 +0100

    esercizi a casa

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         use: {
             loader: "babel-loader",
             options: {
-                presets: ['@babel/preset-env']
+                presets: ['@babel/preset-env'] /*permette di trasformare tutto ES6+ a ES5*/
             }
         }
         }
