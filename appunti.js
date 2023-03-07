23e62844ea18b20380a6c2acb0185d12155c4497
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-07 09:46:32 +0100

    appunti

---
+++ appunti.js
@@ -0,0 +1,11 @@
+/*come scrivere le regExps*/
+
+
+const regExpr = /asd/;
+
+const regExpre = new regExp()
+s = "stringa da analizzare"
+regExp.test(s) //controlla se nella stringa ho corrispondenza
+regExp.exec(s) //torna un array con tutte le occorrenze
+
+//per ogni cosa vai su regExp101.com per vedere tutte le codifiche particolari
