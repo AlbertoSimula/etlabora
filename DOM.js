443c83ccdc36da52bcc56c928e7d753b7e816850
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-07 09:32:43 +0100

    aggiunte cartelle

--- DOM.js
+++
@@ -1,8 +0,0 @@
-var p = document.body.firstElementChild;
-p.style.background = "red";
-
-var s = p.nextElementSibling;
-s.style.background="green";
-
-var t = s.lastElementChild;
-t.style.background = "white";
