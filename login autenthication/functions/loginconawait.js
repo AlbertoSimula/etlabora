c3aee20846daae79a8fa8a82d5ed7e6c406fbaf7
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-08 15:48:41 +0100

    aggiunta cartella

---
+++ login autenthication/functions/loginconawait.js
@@ -0,0 +1,19 @@
+export const login = async (email,password) => {
+
+    const response =  await fetch('https://reqres.in/api/login', {
+    body: JSON.stringify({
+        email,
+        password
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    })
+
+    const data = await response.json();
+    sessionStorage.setItem('user.token', data.token);
+    return data.token
+};
+
+
