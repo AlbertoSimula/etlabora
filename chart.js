9f88cb7db68a388f789cb5c28e721a9bd6b701b1
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-09 15:20:23 +0100

    appunti.js

--- chart.js
+++ chart.js
@@ -2,5 +2,5 @@ import Chart from 'chart.js/auto';
 
 const chart = new Chart(ctx, {
     type: 'line',
-    data: data
+    data: data //evitabile perche è qualcosa che prende i dati da qualcosa di esterno
 });
