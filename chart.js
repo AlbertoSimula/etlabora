b2132d7a6504038f48078e1ccf242621e2136b8b
Alberto Simula <103315974+AlbertoSimula@users.noreply.github.com>
2023-03-10 08:30:17 +0100

    non so cosa

--- chart.js
+++ chart.js
@@ -1,6 +1,26 @@
 import Chart from 'chart.js/auto';
+//funziona?si. è utile? forse no, meglio importarsi solo ciò che mi serve
 
+const ctx = document.querySelector('#chart');
+
+const inputs = {
+    min: -100,
+    max: 100,
+    count: 8,
+    decimals: 2,
+    continuity: 1
+};
+const data = {
+    labels: ["A","B","C","D","E","F","G","H"],
+    datasets: [
+    {
+        label: 'Dataset',
+        data: [-10, 15, 11, 4, 9, 22, 32, -15],
+    }
+    ]
+};
 const chart = new Chart(ctx, {
-    type: 'line',
-    data: data //evitabile perche è qualcosa che prende i dati da qualcosa di esterno
+    data, //evitabile perche è qualcosa che prende i dati da qualcosa di esterno
+    type: 'line'
+    
 });
